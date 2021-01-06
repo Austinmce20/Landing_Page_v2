404a089343c0ac7fd726e5f9cf9e5024d5924d60
Austin McEachern <71673263+Austinmce20@users.noreply.github.com>
2021-01-06 00:41:10 -0800

    Initial commit
    Adding source files

---
+++ script.js
@@ -0,0 +1,7 @@
+function submitScript(){
+    var name = document.getElementById("name").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+
+    alert(name + "\n" + lastName + "\n" + email)
+}
